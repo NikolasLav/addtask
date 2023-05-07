bb76a6802f93fb30d8ff9bf286a431bb834d3905
NikolasLav <116113002+NikolasLav@users.noreply.github.com>
2023-05-07 18:07:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#FF00FF";
 
 let btn = document.getElementById("btn");
+let task_name = document.getElementById("task_name");
 
 btn.addEventListener("click", function(){
-    tg.MainButton.setText("Сообщение отправлено!");
+    tg.MainButton.setText("Задача создана!");
     tg.MainButton.show();
-    tg.sendData("sendTestMessage");
+
+    tg.sendData(task_name);
 });
